916892fd98150cf4be5b317b3a7b7e1ce2afa891
bigcl <1748684650@qq.com>
2022-05-03 11:30:35 +0800

    二分查找法

---
+++ erfenchazhao/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int erfenchazhao(int num,int* arry,int length);
+int main() {
+    int arry[11] = {5,13,19,21,37,56,64,75,80,88,92};
+    printf("21дкЃК%d\n",erfenchazhao(21,arry,11));
+    printf("85дк: %d",erfenchazhao(85,arry,11));
+}
+
+int erfenchazhao(int num,int* arry,int length){
+    int left = 0,right = length - 1;
+    int mid = (left + right)/2;
+    while(left <= right){
+        if(num < arry[mid]){
+            right = mid - 1;
+            mid = (left + right) / 2;
+        }
+        else if(num > arry[mid]){
+            left = mid + 1;
+            mid = (left + right) / 2;
+        } else return mid + 1;
+    }
+    return 0;
+}
